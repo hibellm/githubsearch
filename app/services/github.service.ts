7a10bf07e353c152ea6a68e376eac12c0fb58bb0
Marcus <marcus.hibell@roche.com>
2017-08-06 00:25:47 +0200

    updating

--- app/services/github.service.ts
+++ app/services/github.service.ts
@@ -4,24 +4,24 @@ import 'rxjs/add/operator/map';
 
 @Injectable()
 export class GithubService{
-    private username = 'bradtraversy';
-    private client_id = 'd9308aacf8b204d361fd';
-    private client_secret='62551cc02cee983fff0bac41baf170eb5a312c1c';
-    
+    private username = 'hibellm';
+    private client_id = 'e19e0df62f4b0af4efab';
+    private client_secret='12a41134aac11ded8961a538047964a56f49b821';
+
     constructor(private _http:Http){
         console.log('Github Service Init...');
     }
-    
+
     getUser(){
         return this._http.get('https://api.github.com/users/'+this.username+'?client_id='+this.client_id+'&client_secret='+this.client_secret)
             .map(res => res.json());
     }
-    
+
     getRepos(){
         return this._http.get('https://api.github.com/users/'+this.username+'/repos?client_id='+this.client_id+'&client_secret='+this.client_secret)
             .map(res => res.json());
     }
-    
+
     updateUsername(username:string){
         this.username = username;
     }
